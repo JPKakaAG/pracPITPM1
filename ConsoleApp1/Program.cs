68763988438e0ea1a62860cf78afced207707358
исп-41 <-41@LAB24-04>
2024-11-09 08:55:07 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите A");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine($"{Var5.Class1.Fact(A)}");
+Console.ReadLine();
---
+++ Var5/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace Var5
+{
+    public class Class1
+    {
+        public static int Fact(int A)
+        {
+            int F = 1;
+            for (int i = 0; i <= A; i++)
+            {
+                F = F * i;
+            }
+            return F;
+        }
+    }
+}
